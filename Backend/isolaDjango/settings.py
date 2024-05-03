04b652588f4260c8b5d30a0fb985c0091009d18d
alesierra <alesierraalta@gmail.com>
2024-05-03 11:16:17 -0400

    v0.18.13 vercel files

--- Backend/isolaDjango/settings.py
+++ Backend/isolaDjango/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']  # Asegúrate de ajustarlo correctamente para producción
+
 
 
 # Application definition

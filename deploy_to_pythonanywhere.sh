a050c9db4b5cae9ba0dd715f50bdc499a6cc0931
Alejandro Sierraalta <117122488+alesierraalta@users.noreply.github.com>
2024-05-15 07:04:09 -0400

    Update deploy_to_pythonanywhere.sh

--- deploy_to_pythonanywhere.sh
+++ deploy_to_pythonanywhere.sh
@@ -21,13 +21,13 @@ echo "${PYTHONANYWHERE_SSH_KEY}" | tr -d '\r' > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 
 # Desempaquetar el tarball en PythonAnywhere y recargar la aplicación web
-ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no ${PA_USER}@ssh.pythonanywhere.com << EOF
-  cd ${PROJECT_DIR}
-  tar xzf /home/${PA_USER}/${REPO_NAME}.tar.gz -C ${PROJECT_DIR}
+ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -T ${PA_USER}@ssh.pythonanywhere.com << 'EOF'
+  cd /home/${PA_USER}/IsolaMenu
+  tar xzf /home/${PA_USER}/${REPO_NAME}.tar.gz -C /home/${PA_USER}/IsolaMenu
   rm /home/${PA_USER}/${REPO_NAME}.tar.gz
   source /home/${PA_USER}/.virtualenvs/your_virtualenv/bin/activate  # Activa tu entorno virtual
-  pip install -r ${PROJECT_DIR}/Backend/requirements.txt
-  python ${PROJECT_DIR}/Backend/manage.py collectstatic --noinput
-  python ${PROJECT_DIR}/Backend/manage.py migrate
+  pip install -r /home/${PA_USER}/IsolaMenu/Backend/requirements.txt
+  python /home/${PA_USER}/IsolaMenu/Backend/manage.py collectstatic --noinput
+  python /home/${PA_USER}/IsolaMenu/Backend/manage.py migrate
   pa_reload_webapp your_webapp_name.pythonanywhere.com
 EOF
